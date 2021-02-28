85bf70ebc822a2cff204ae8e60389137476a8481
Julian Fortune <fortunej@oregonstate.edu>
2021-02-27 20:22:15 -0800

    Progress on Signal Extraction

--- Extraction.py
+++ Extraction.py
@@ -2,6 +2,7 @@ import cv2
 import matplotlib.pyplot as plt
 from cv2 import imread as loadImage
 from cv2 import imwrite as writeImage
+import numpy as np
 
 from src.main.python.extraction import Binarization, SignalExtraction
 
@@ -15,11 +16,28 @@ def showColorImage(image):
     plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
     # plt.show()
 
+def openImage(binaryImage):
+    # Open the image
+    element = cv2.getStructuringElement(cv2.MORPH_OPEN, (3,3))
+    eroded = cv2.erode(binaryImage, element)
+    eroded = cv2.erode(eroded, element)
+    element = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3))
+    opened = cv2.dilate(eroded, element)
+
+    return opened
+
+def smoothEdges(binaryImage):
+    element = cv2.getStructuringElement(cv2.MORPH_DILATE, (3,3))
+    smoothed = cv2.erode(binaryImage, element)
+
+    return smoothed
+
 path = "leadPictures/slighty-noisey-aVL-small.png"
 # path = "leadPictures/slighty-noisey-aVL.png"
+# path = "leadPictures/fullscan-I.png"
 # path = "data/1480/1480_Page1.jpg"
 
-# path = "../Test Images/Mocks/test2.png"
+path = "../Test Images/Mocks/test2.png"
 # path = "../Test Images/Larisa/ZEL297(9).jpg"
 # path = "data/700px-AMI_inferior_2.jpg"
 
@@ -27,16 +45,18 @@ testImage = loadImage(path)
 
 binaryImage = Binarization.mallawaarachchiBasic(testImage, useBlur=True)
 
-showGreyscaleImage(binaryImage)
 # plt.show()
 
-#signal = SignalExtraction.horizontalScanLookBehind(binaryImage)
-signal = SignalExtraction.horizontalScanLookBehind(binaryImage)
+# processedImage = openImage(binaryImage)
 
-plt.imshow(signal, cmap='spring')
-plt.show()
+showGreyscaleImage(binaryImage)
+
+signal = SignalExtraction.traceLines(binaryImage)
+
+# plt.imshow(signal, cmap='spring')
+# plt.show()
 
 # print(signal)
 
-# plt.plot(signal, c='blueviolet', linewidth=2)
-# plt.show()
+plt.plot(signal, c='blueviolet', linewidth=2)
+plt.show()
--- src/main/python/extraction/SignalExtraction.py
+++ src/main/python/extraction/SignalExtraction.py
@@ -6,10 +6,21 @@ Created February 17, 2021
 """
 
 
-from typing import Any, Callable, Generator, Iterable, Optional, Tuple
+from typing import Any, Callable, Generator, Iterable, Optional, Tuple, TypeVar, Union
 import numpy as np
 from math import sqrt, asin, pi
-from collections import namedtuple
+from heapq import heapify, heappop, heappush, heapreplace
+from functools import partial
+
+import matplotlib.pyplot as plt
+
+List = list
+Int = int
+Float = float
+
+A = TypeVar("A")
+B = TypeVar("B")
+Numeric = Union[Float, Int]
 
 def reversedRange(stop):
         return range(stop-1, -1, -1)
@@ -17,13 +28,31 @@ def reversedRange(stop):
 def inclusiveRange(start, stop):
     return range(start, stop+1)
 
-def neg(inputValue: int) -> int:
+def neg(inputValue: Int) -> Int:
     return -1 * inputValue
+
+def upperClamp(value: Numeric, limit: Numeric) -> Numeric:
+    return value if (value < limit) else limit
+
+def lowerClamp(value: Numeric, limit: Numeric) -> Numeric:
+    return value if (value > limit) else limit
+
+def mapList(elements: Iterable[A], func: Callable[[A], B]) -> Iterable[B]:
+    return list(map(func, elements))
+
+def flatten(listOfLists: Iterable[Iterable[A]]) -> Iterable[A]:
+    return [e for _list in listOfLists for e in _list]
+
+def flatMap(elements: Iterable[A], func: Callable[[A], Iterable[Iterable[B]]]) -> Iterable[B]:
+    return flatten(mapList(elements, func))
+
+def filterList(elements: Iterable[A], func: Callable[[A], A]) -> Iterable[A]:
+    return list(filter(func, elements))
 
 
 def findFirstLastNonZeroPixels(oneDimImage: np.ndarray) -> Tuple[int, int]:
 
-    def reverseEnumerate(array: np.ndarray):
+    def reverseEnumerate(array: np.ndarray) -> Generator[int,Any]:
         for index in reversedRange(len(array)):
             yield index, array[index]
 
@@ -80,7 +109,7 @@ def findContiguousRegionCenters(oneDimImage: np.ndarray) -> Iterable[int]:
 def euclideanDistance(x: int, y: int) -> int:
     return sqrt((x**2) + (y**2))
 
-hypotenuse: Callable[[int,int], int] = euclideanDistance
+hypotenuse = euclideanDistance
 
 def distanceBetweenPoints(firstPoint: Tuple[int, int], secondPoint: Tuple[int, int]) -> int:
     return euclideanDistance(firstPoint[0] - secondPoint[0], firstPoint[1] - secondPoint[1])
@@ -97,7 +126,7 @@ def angleBetweenPoints(firstPoint: Tuple[int, int], secondPoint: Tuple[int, int]
 
 
 def angleSimilarity(firstAngle: float, secondAngle: float) -> float:
-    return abs(secondAngle - firstAngle)
+    return (180 - abs(secondAngle - firstAngle)) / 180
 
 
 def searchArea(initialRow: Tuple[int, int], radius: int) -> Iterable[Tuple[int]]:
@@ -114,61 +143,183 @@ def searchArea(initialRow: Tuple[int, int], radius: int) -> Iterable[Tuple[int]]
 def getPointLocations(image: np.ndarray) -> np.ndarray:
     columns = np.swapaxes(image, 0, 1)
 
-    pointLocations = np.full(image.shape, np.nan)
+    pointLocations = []
 
     # Scan horizontally across the image
-    for columnIndex, column in enumerate(columns):
+    for column, pixels in enumerate(columns):
         # Get all points that could be part of a signal
-        points = findContiguousRegionCenters(column)
+        rows = findContiguousRegionCenters(pixels)
+        points = mapList(rows, lambda row: (row, column))
 
-        for point in points:
-            pointLocations[point, columnIndex] = 1
+        pointLocations.append(points)
 
     return pointLocations
-
-
-class SignalPoint():
-
-    def __init__(self, x: int, y: int, orientation: float) -> None:
-        pass
 
 
 class SignalBuilder():
+    DEFAULT_ORIENTATION: float = 0.0
+    DISTANCE_WEIGHT: float = 0.2
 
-    def __init__(self) -> None:
-        self.pointList = []
+    assert DISTANCE_WEIGHT <= 1 and DISTANCE_WEIGHT >= 0
+
+    def __init__(self, radius, orientationSmoothing) -> None:
+        self.points: Iterable[Tuple[int,int]] = []
+        self.orientations: Iterable[float] = []
+        self.orientationSmoothing: int = orientationSmoothing
+        self.radius: int = radius
+
+        self.cacheValidationFinalOrientation: float = None # This tells us if the cache is valid
+        self.cachedAveragedOrientation: float = None
 
     @classmethod
-    def startingAt(thisClass, point: Tuple[int,int]):
-        x, y = point
+    def startingAt(thisClass, x: int, y: int, radius: int, orientationSmoothing: int = 0):
+        newInstance = thisClass(radius, orientationSmoothing)
+        point = (x,y)
+        newInstance.addPoint(point)
+        return newInstance
 
-    @staticmethod
-    def evaluate(first: SignalPoint, second: SignalPoint):
-        pass
+    @property
+    def cacheIsValid(self):
+        return self.orientations[-1] == self.cacheValidationFinalOrientation
+
+    def calculateAverageOrientation(self) -> Optional[float]:
+        count = len(self.orientations)
+        if count == 0: return None
+
+        span = upperClamp(self.orientationSmoothing, count)
+        return np.mean(self.orientations[neg(span + 1):])
+
+    @property
+    def currentOrientation(self):
+        if not self.cacheIsValid:
+            self.cachedAveragedOrientation = self.calculateAverageOrientation()
+
+        self.cacheValidationFinalOrientation = self.orientations[-1]
+        return self.cachedAveragedOrientation
+
+    def likelihoodOfBeingInSignal(self, point: Tuple[int,int]):
+        angle = angleBetweenPoints(self.lastPoint, point)
+        angleValue = angleSimilarity(angle, self.currentOrientation)
+        distanceValue = (self.radius - distanceBetweenPoints(self.lastPoint, point)) / self.radius
+        return (distanceValue * self.DISTANCE_WEIGHT) + (angleValue * (1 - self.DISTANCE_WEIGHT))
+
+    @property
+    def lastPoint(self) -> Optional[Tuple[Int, Int]]:
+        return self.points[-1] if len(self.points) else None
+
+    def addPoint(self, newPoint: Tuple[int,int]):
+        if len(self.points) == 0:
+            self.orientations.append(self.DEFAULT_ORIENTATION)
+        else:
+            newOrientation = angleBetweenPoints(self.points[-1], newPoint)
+            self.orientations.append(newOrientation)
+
+        self.points.append(newPoint)
 
 
-def horizontalScanLookBehind(image: np.ndarray, radius: int = 12, orientationSmoothing: int = 0):
-    columns = np.swapaxes(image, 0, 1)
+def batch(elements: Iterable[A], batchSize: Int, startAtEveryElement: bool = False, shouldFlatten: bool = True):  # -> Generator[Iterable[A], ???]:
+    listLength = len(elements)
+    startIndices = range(len(elements) if startAtEveryElement else (len(elements) - batchSize))
+    for index in startIndices:
+        end = upperClamp(index + batchSize, listLength)
+        if shouldFlatten:
+            yield flatten(elements[index:end])
+        else:
+            yield elements[index:end]
 
+
+def traceLines(image: np.ndarray, radius: int = 12, orientationSmoothing: int = 4):
     pointLocations = getPointLocations(image)
+    claimedPoints = set()
+    signalBuilders: Iterable[SignalBuilder] = []
 
-    return pointLocations
+    # pointLocationMatrix = np.zeros_like(image)
+    # for row, column in flatten(pointLocations):
+    #     pointLocationMatrix[row][column] = 1
+    # plt.imshow(pointLocationMatrix, cmap='spring')
+    # plt.show()
+
+    for column, (points, thisColumnPoints) in enumerate(
+        zip(batch(pointLocations, batchSize=radius, startAtEveryElement=True), pointLocations)
+    ):
+        buildersByPriority = []
+
+        # print(f"=== Column {column} ===")
+        # print(points)
+
+        for builderNumber, builder in enumerate(signalBuilders):
+            # [print(distanceBetweenPoints(builder.lastPoint, point)) for point in points]
+
+            possiblePoints = filterList(points, lambda point: distanceBetweenPoints(builder.lastPoint, point) <= radius and point[1] == column)
+
+            if len(possiblePoints) == 0:
+                continue
+
+            likelihoods = mapList(possiblePoints, builder.likelihoodOfBeingInSignal)
+
+            # Put all the (likelihood, point) pairs in a maxheap
+            priorities = mapList(likelihoods, neg)
+            bestMatches = list(zip(priorities, possiblePoints))
+            heapify(bestMatches)
+
+            # print(builderNumber, bestMatches)
+
+            # If the best match is not the current column, hold off until the next column is processed
+            if bestMatches[0][1][1] != column:
+                continue
+
+            # `builderNumber` is added as an arbitrary tie-breaker so heapq doesn't try to compare builders with `>`
+            heappush(buildersByPriority, (bestMatches[0][0], builderNumber, bestMatches, builder))
+
+        # [print(p) for p in buildersByPriority]
+
+        while len(buildersByPriority) > 0:
+            _, builderNumber, bestMatches, builder = buildersByPriority[0]
+
+            # Ignore any builders that are out of points to match with
+            if len(bestMatches) == 0:
+                heappop(buildersByPriority)
+                continue
+
+            _, bestPoint = heappop(bestMatches)
+
+            # Builder's best point is already taken, try the next best
+            if bestPoint in claimedPoints:
+                # If there is another point and it's still in the current column try that point instead
+                if len(bestMatches) > 0 and bestMatches[0][1][1] == column:
+                    heapreplace(buildersByPriority, (bestMatches[0][0], builderNumber, bestMatches, builder))
+                else:
+                    heappop(buildersByPriority)
+
+            if bestPoint not in claimedPoints:
+                claimedPoints.add(bestPoint)
+                builder.addPoint(bestPoint)
+
+                # This builder has gotten its point for the column
+                heappop(buildersByPriority)
+
+        unclaimedPoints = filterList(thisColumnPoints, lambda x: x not in claimedPoints)
+
+        for row, column in unclaimedPoints:
+            print(f"Creating new signal for ({row},{column})")
+            signalBuilders.append(SignalBuilder.startingAt(row, column, radius, orientationSmoothing))
+            claimedPoints.add((row, column))
+
+    # Select the best signal
+    signalSizes = mapList(signalBuilders, lambda b: len(b.points))
+    bestSignal = signalBuilders[np.core.fromnumeric.argmax(signalSizes)]
+
+    signal = np.full(image.shape[1], np.nan)
+
+    for row, column in bestSignal.points:
+        signal[column] = row
+
+    return signal
+
+
+if __name__ == "__main__":
+    pass
 
 
 # TODO
-
 # * Implement function for determining likelihood of point being in signal (weight proximity higher)
-# * Implement line tracing function
-#   - Use the 2d array showing potential point locations
-#   - Figure out how to start lines (any unclaimed point after running through all signals?)
-#   - Iterate over all signals
-#      - Find all points in search space
-#      - Evaluate likelihood of each point being part of the line
-#      - Put all the (likelihood, point) pairs in a maxheap
-#      - Create method to spit out points the signal "wants" in order of likelihood
-#   - Keep track of claimed points
-#   - Put all (likelihood of best point, signal) pairs in maxheap
-#   - Start allocating points to signals
-#   - If a signal tries to claim a claimed point, ask it for it's next favorite point and changekey in the maxheap
-
-# * Implement function for creating a 2d default dict for tracking claimed points (overkill?)
